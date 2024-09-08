6e1fcd4d518d17fa4cee9e7b78dcc2bd238bc1ff
Ulric Huot <ulric.huot@gmail.com>
2024-09-08 00:45:38 -0400

    Removed hardcoded test data

--- src/public/ts/index.ts
+++ src/public/ts/index.ts
@@ -1,18 +1,7 @@
-import { API } from "./api.js"
 import { Budget } from "./budget.js"
-import { PaymentMethod } from "./entries/payment-method.js"
-import { Person } from "./entries/person.js"
 import { create, Icons } from "./utils.js"
 
 export const budget = new Budget()
-
-const uuids = new Array(4).fill(null).map(() => crypto.randomUUID())
-budget.people.set(uuids[0], new Person(budget, { uuid: uuids[0], name: 'Nakitia' }))
-budget.people.set(uuids[1], new Person(budget, { uuid: uuids[1], name: 'Ulric' }))
-budget.paymentMethods.set(uuids[2], new PaymentMethod(budget, { uuid: uuids[2], name: 'TD Debit ****1463', owner_uuid: uuids[0] }))
-budget.paymentMethods.set(uuids[3], new PaymentMethod(budget, { uuid: uuids[3], name: 'TD Credit ****9579', owner_uuid: uuids[1] }))
-
-budget.refreshAll()
 
 document.getElementById('root')?.append(
    create('h2', { class: 'fit' }, [Icons.BarChart, ' Summary']),
