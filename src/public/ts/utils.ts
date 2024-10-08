ea4b09c3ed4895ff22c61fb11f878d800834884d
Ulric Huot <ulric.huot@gmail.com>
2024-10-07 23:07:00 -0400

    Added blendColors function

--- src/public/ts/utils.ts
+++ src/public/ts/utils.ts
@@ -28,6 +28,15 @@ export function formatMoney(money: number, positiveSign?: boolean) {
    return (positiveSign && money >= 0 ? '+' : '') + money.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
 }
 
+export function blendColors(colorA: string, colorB: string, amount: number) {
+   const [rA, gA, bA] = colorA.match(/\w\w/g)?.map((c) => parseInt(c, 16)) ?? [0, 0, 0];
+   const [rB, gB, bB] = colorB.match(/\w\w/g)?.map((c) => parseInt(c, 16)) ?? [0, 0, 0];
+   const r = Math.round(rA + (rB - rA) * amount).toString(16).padStart(2, '0');
+   const g = Math.round(gA + (gB - gA) * amount).toString(16).padStart(2, '0');
+   const b = Math.round(bA + (bB - bA) * amount).toString(16).padStart(2, '0');
+   return '#' + r + g + b;
+}
+
 export class Tooltips {
    static list: bootstrap.Tooltip[] = []
 
