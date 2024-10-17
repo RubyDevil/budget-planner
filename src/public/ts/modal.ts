f8d5b20b2e09d71b18689074f1b888da639cfea4
Ulric Huot <ulric.huot@gmail.com>
2024-10-17 03:32:44 -0400

    Added utils function withFloatingLabel

--- src/public/ts/modal.ts
+++ src/public/ts/modal.ts
@@ -13,10 +13,10 @@ export class Modal {
 
    constructor(title: string, dismissable: boolean, ephemeral: boolean = true) {
       this.root = document.body.appendChild(create('div', { class: 'modal fade', tabindex: -1, 'data-bs-backdrop': dismissable || 'static', 'data-bs-keyboard': dismissable }))
-      this.dialog = this.root.appendChild(create('div', { class: 'modal-dialog' }))
+      this.dialog = this.root.appendChild(create('div', { class: 'modal-dialog modal-dialog-centered modal-dialog-scrollable' }))
       this.content = this.dialog.appendChild(create('div', { class: 'modal-content' }))
       this.header = this.content.appendChild(create('div', { class: 'modal-header' }))
-      this.body = this.content.appendChild(create('div', { class: 'modal-body' }))
+      this.body = this.content.appendChild(create('div', { class: 'modal-body container-fluid' }))
       this.footer = this.content.appendChild(create('div', { class: 'modal-footer' }))
       this.title = this.header.appendChild(create('h5', { class: 'modal-title' }, title))
       this.#closeButton = this.header.appendChild(create('button', { type: 'button', class: 'btn-close', 'data-bs-dismiss': 'modal', 'aria-label': 'Close' }))
