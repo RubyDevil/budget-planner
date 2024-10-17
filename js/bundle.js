a4c0f72a44695beae884b6d7e32ee923e58724b0
RubyDevil <ulric.huot@gmail.com>
2024-10-17 22:13:17 +0000

    Deploying to gh-pages from @ RubyDevil/budget-planner@032f4a6dd3b68931f62181ac174a53df96054654 🚀

--- js/bundle.js
+++ js/bundle.js
@@ -652,7 +652,7 @@
       const originalDays = CYCLE_DAYS[this.billing_cycle[1]] * this.billing_cycle[0];
       const total = this.amount * targetDays / originalDays;
       const percentage = targetPerson ? this.payers.get(targetPerson.uuid) ?? 0 : 1;
-      return Math.ceil(total * percentage * 100) / 100;
+      return Math.ceil(total * percentage) / 100;
     }
     createLink() {
       return create("a", { class: "text-primary" }, [Icons.Bidirectional, " " + this.name]);
