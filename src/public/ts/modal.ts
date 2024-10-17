bc5e677c84d71341291d0d40b897f6ac7edddd51
Ulric Huot <ulric.huot@gmail.com>
2024-10-17 04:57:52 -0400

    Added optionnal callback argument to Modal.rebuild

--- src/public/ts/modal.ts
+++ src/public/ts/modal.ts
@@ -11,7 +11,7 @@ export abstract class Modal {
    static #closeButton: HTMLButtonElement = this.header.appendChild(create('button', { type: 'button', class: 'btn-close', 'data-bs-dismiss': 'modal', 'aria-label': 'Close' })) /* @ts-ignore */
    static #bsInstance: bootstrap.Modal = new bootstrap.Modal(this.root)
 
-   static rebuild(title: string, dismissable: boolean) {
+   static rebuild(title: string, dismissable: boolean, callback?: () => any) {
       this.root = document.body.appendChild(create('div', { class: 'modal fade', tabindex: -1, 'data-bs-backdrop': dismissable || 'static', 'data-bs-keyboard': dismissable }))
       this.dialog = this.root.appendChild(create('div', { class: 'modal-dialog modal-dialog-centered modal-dialog-scrollable' }))
       this.content = this.dialog.appendChild(create('div', { class: 'modal-content' }))
@@ -23,6 +23,7 @@ export abstract class Modal {
       if (!dismissable) this.#closeButton.remove()
       this.#bsInstance.dispose() /* @ts-ignore */
       this.#bsInstance = new bootstrap.Modal(this.root)
+      this.root.addEventListener('hidden.bs.modal', () => callback?.())
    }
 
    static show() { this.#bsInstance.show() }
