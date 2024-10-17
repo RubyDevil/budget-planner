a42ce44e81faa6d82aac58b7cba318f7712325d8
Ulric Huot <ulric.huot@gmail.com>
2024-10-17 09:37:14 -0400

    Modified initial budget data

--- src/public/ts/index.ts
+++ src/public/ts/index.ts
@@ -14,10 +14,10 @@ const paymentMethodCash = new PaymentMethod(budget, { uuid: crypto.randomUUID(),
 const paymentMethodBankAccount = new PaymentMethod(budget, { uuid: crypto.randomUUID(), name: 'Bank Account', owner_uuid: personMe.uuid })
 const categorySalaries = new Category(budget, { uuid: crypto.randomUUID(), icon: Icons.CashStack.className, name: 'Salaries', color: '#00ff00' })
 const categoryBills = new Category(budget, { uuid: crypto.randomUUID(), icon: Icons.Card.className, name: 'Bills', color: '#ff0000' })
-const transactionSalary = new Transaction(budget, { uuid: crypto.randomUUID(), category_uuid: categorySalaries.uuid, name: 'Salary', amount: 5432.10, payment_method_uuid: paymentMethodBankAccount.uuid, billing_cycle: [1, CYCLE.MONTH], payers: {} })
+const transactionSalary = new Transaction(budget, { uuid: crypto.randomUUID(), category_uuid: categorySalaries.uuid, name: 'Salary', amount: 5432.10, payment_method_uuid: paymentMethodBankAccount.uuid, billing_cycle: [1, CYCLE.MONTH], payers: { [personMe.uuid]: 100 } })
 const transactionRent = new Transaction(budget, { uuid: crypto.randomUUID(), category_uuid: categoryBills.uuid, name: 'Rent', amount: -1234.56, payment_method_uuid: paymentMethodCash.uuid, billing_cycle: [1, CYCLE.MONTH], payers: { [personMe.uuid]: 50, [personOther.uuid]: 50 } })
-const transactionUnknownIncome = new Transaction(budget, { uuid: crypto.randomUUID(), category_uuid: '...', name: 'Misc (Unknown)', amount: 1500, payment_method_uuid: paymentMethodCash.uuid, billing_cycle: [1, CYCLE.MONTH], payers: {} })
-const transactionUnknownExpense = new Transaction(budget, { uuid: crypto.randomUUID(), category_uuid: '...', name: 'Misc (Unknown)', amount: -150, payment_method_uuid: paymentMethodCash.uuid, billing_cycle: [1, CYCLE.MONTH], payers: {} })
+const transactionUnknownIncome = new Transaction(budget, { uuid: crypto.randomUUID(), category_uuid: '...', name: 'Misc (Unknown)', amount: 1500, payment_method_uuid: paymentMethodCash.uuid, billing_cycle: [1, CYCLE.MONTH], payers: { [personMe.uuid]: 100 } })
+const transactionUnknownExpense = new Transaction(budget, { uuid: crypto.randomUUID(), category_uuid: '...', name: 'Misc (Unknown)', amount: -150, payment_method_uuid: paymentMethodCash.uuid, billing_cycle: [1, CYCLE.MONTH], payers: { [personOther.uuid]: 100 } })
 
 budget.people.set(personMe.uuid, personMe)
 budget.people.set(personOther.uuid, personOther)
