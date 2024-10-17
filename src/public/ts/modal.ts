e8d54653b8fcf0f6aa330e983c57d2430e02b82f
Ulric Huot <ulric.huot@gmail.com>
2024-10-17 02:45:53 -0400

    Added Modal class

---
+++ src/public/ts/modal.ts
@@ -0,0 +1,37 @@
+import { create } from "./utils"
+
+export class Modal {
+   #bsInstance: bootstrap.Modal
+   root: HTMLDivElement
+   dialog: HTMLDivElement
+   content: HTMLDivElement
+   header: HTMLDivElement
+   body: HTMLDivElement
+   footer: HTMLDivElement
+   title: HTMLHeadingElement
+   #closeButton: HTMLButtonElement
+
+   constructor(title: string, dismissable: boolean, ephemeral: boolean = true) {
+      this.root = document.body.appendChild(create('div', { class: 'modal fade', tabindex: -1, 'data-bs-backdrop': dismissable || 'static', 'data-bs-keyboard': dismissable }))
+      this.dialog = this.root.appendChild(create('div', { class: 'modal-dialog' }))
+      this.content = this.dialog.appendChild(create('div', { class: 'modal-content' }))
+      this.header = this.content.appendChild(create('div', { class: 'modal-header' }))
+      this.body = this.content.appendChild(create('div', { class: 'modal-body' }))
+      this.footer = this.content.appendChild(create('div', { class: 'modal-footer' }))
+      this.title = this.header.appendChild(create('h5', { class: 'modal-title' }, title))
+      this.#closeButton = this.header.appendChild(create('button', { type: 'button', class: 'btn-close', 'data-bs-dismiss': 'modal', 'aria-label': 'Close' }))
+      if (!dismissable) this.#closeButton.remove()
+      /* @ts-ignore */
+      this.#bsInstance = new bootstrap.Modal(this.root)
+      if (ephemeral) this.root.addEventListener('hidden.bs.modal', () => this.destroy())
+   }
+
+   show() { this.#bsInstance.show() }
+   hide() { this.#bsInstance.hide() }
+   toggle() { this.#bsInstance.toggle() }
+
+   destroy() {
+      this.#bsInstance.dispose()
+      this.root.remove()
+   }
+}
